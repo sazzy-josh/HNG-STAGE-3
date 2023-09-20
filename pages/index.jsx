0823f5f21a40856d8e58c3ade8ed17db49862912
Idahosa Joshua <idahosa@bento.africa>
2023-09-20 21:48:38 +0100

    fix: fixed deployment issue

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,6 @@
 import Head from "next/head";
 import {Inter} from "next/font/google";
-import ProtectedRoute from "@/components/ProtectedRoute";
 import Login from "@/components/Form";
-import {useContext, useEffect} from "react";
-import {AuthContext} from "@/context/auth";
 
 const inter = Inter({subsets: ["latin"]});
 
