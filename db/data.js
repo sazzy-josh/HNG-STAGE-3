5acdc35101219d67b325de73da6218e5ce383006
Idahosa Joshua <idahosa@bento.africa>
2023-09-21 07:46:22 +0100

    added more mock data

--- db/data.js
+++ db/data.js
@@ -40,42 +40,42 @@ export const imagesDb = [
     src: "/images/monkey4.jpg",
     tag: "NFT",
   },
-  //   {
-  //     id: 8,
-  //     src: "/images/monkey1.png",
-  //     tag: "NFT",
-  //   },
-  //   {
-  //     id: 10,
-  //     src: "/images/monkey3.jpg",
-  //     tag: "NFT",
-  //   },
+  {
+    id: 8,
+    src: "/images/monkey1.png",
+    tag: "NFT",
+  },
+  {
+    id: 10,
+    src: "/images/monkey3.jpg",
+    tag: "NFT",
+  },
 
-  //   {
-  //     id: 12568,
-  //     src: "/images/monkey2.jpg",
-  //     tag: "NFT",
-  //   },
-  //   {
-  //     id: 16,
-  //     src: "/images/monkey4.jpg",
-  //     tag: "NFT",
-  //   },
+  {
+    id: 12568,
+    src: "/images/monkey2.jpg",
+    tag: "NFT",
+  },
+  {
+    id: 16,
+    src: "/images/monkey4.jpg",
+    tag: "NFT",
+  },
 
-  //   {
-  //     id: 120909,
-  //     src: "/images/monkey2.jpg",
-  //     tag: "NFT",
-  //   },
-  //   {
-  //     id: 16900,
-  //     src: "/images/woman1.jpg",
-  //     tag: "woman",
-  //   },
+  {
+    id: 120909,
+    src: "/images/monkey2.jpg",
+    tag: "NFT",
+  },
+  {
+    id: 16900,
+    src: "/images/woman1.jpg",
+    tag: "woman",
+  },
 
-  //   {
-  //     id: 5000,
-  //     src: "/images/monkey3.jpg",
-  //     tag: "NFT",
-  //   },
+  {
+    id: 5000,
+    src: "/images/monkey3.jpg",
+    tag: "NFT",
+  },
 ];
